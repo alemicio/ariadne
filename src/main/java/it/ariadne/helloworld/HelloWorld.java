51c1da75f63ffb9e91456d9ea53a1da1756c8de9
alemicio <alemicio91@gmail.com>
2017-02-02 15:55:03 +0100

    second modificascion

--- src/main/java/it/ariadne/helloworld/HelloWorld.java
+++ src/main/java/it/ariadne/helloworld/HelloWorld.java
@@ -1,11 +1,83 @@
 package it.ariadne.helloworld;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.println("helloWorld ! \n");
-
+	List<String> lista = new LinkedList<>();
+	
+	lista.add("uno");
+	lista.add("due");
+	lista.add("tre");
+	
+	System.out.println(lista);
+	
+	Map<String, Persona> rubrica = new HashMap<>();
+	Persona p1 = new Persona();
+	Persona p2 = new Persona();
+	Persona p3 = new Persona();
+	
+	p1.setCodicefiscale("GTTLSN91E09G388U");
+	p2.setCodicefiscale("PPPLSN91E09G388U");
+	p3.setCodicefiscale("CCCLSN91E09G388U");
+	
+	p1.setNome("Emily");
+	p1.setCognome("pene");
+	
+	rubrica.put("adriana", p1);
+	rubrica.put("simo", p2);
+	
+	rubrica.forEach( (k,v) -> System.out.println(k + ": " + v));
+	
+	for (Iterator iterator = rubrica.keySet().iterator(); iterator.hasNext();) {
+		String key = (String) iterator.next();
+		Persona value = rubrica.get(key);
+		System.out.println(value);
+	}
+	
 	}
 
+}
+
+class Persona{
+	private String cognome;
+	private String nome;
+	private String codicefiscale;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Persona [cognome=" + cognome + ", nome=" + nome + ", codicefiscale=" + codicefiscale + "]";
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCodicefiscale() {
+		return codicefiscale;
+	}
+	public void setCodicefiscale(String codicefiscale) {
+		this.codicefiscale = codicefiscale;
+	}
 }
